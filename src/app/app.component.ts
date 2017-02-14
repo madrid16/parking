57efe7a4e26011ad0fde53fa7a0c94dff046f967
Edgar Munoz <edgar.munoz@latam.com>
2017-02-14 11:02:34 -0300

    add boostrap and table html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  cars = [
+    {registration: "AAAA11", dateTime: "15:30"},
+    {registration: "BBBB22", dateTime: "15:30"},
+    {registration: "CCCC33", dateTime: "15:30"},
+    {registration: "DDDD44", dateTime: "15:30"}
+  ];
+
 }
