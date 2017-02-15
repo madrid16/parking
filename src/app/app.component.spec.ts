88819976e065cdf8646eee1288bfb1c3b6da3989
Edgar Munoz <edgar.munoz@latam.com>
2017-02-15 10:38:53 -0300

    refactor pipe filter with test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,18 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async } from '@angular/core/testing';
+import {TestBed, async} from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import {RegistrationPipe} from "../pipe/filter.pipe";
 
 describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        RegistrationPipe
       ],
+      imports: [ FormsModule ]
     });
     TestBed.compileComponents();
   });
@@ -19,16 +23,26 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('h1').textContent).toContain('LynCar');
   }));
+
+  it('should be contain four tr on table', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    let nativeElement = fixture.nativeElement;
+    expect(nativeElement.querySelectorAll('tbody tr').length).toBe(4);
+  }));
+
+
+  it('should filter table string with one and zero result', async(() => {
+    let pipe: RegistrationPipe = new RegistrationPipe();
+    let cars: string = JSON.stringify({registration: "FGJG15", dateTime: "13:13"});
+    expect(pipe.transform([JSON.parse(cars)], "FG").length).toBe(1);
+    expect(pipe.transform([JSON.parse(cars)], "AA").length).toBe(0);
+  }));
+
 });
--- src/pipe/filter.pipe.ts
+++ src/pipe/filter.pipe.ts
@@ -1,15 +1,15 @@
-import {Pipe} from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({
-  name: 'AgePipe'
+  name: 'RegistrationPipe'
 })
 
-export class AgePipe {
+export class RegistrationPipe implements PipeTransform{
 
-  transform(input, args?) {
+  transform(input:Array<JSON>, args?:string) {
     let regex = new RegExp(args, "i");
     return input.filter(car => {
-      return (car.registration.match(regex)) ? car.registration.match(regex).input : "";
+      return (car['registration'].match(regex)) ? car['registration'].match(regex).input : "";
     });
   }
 }
